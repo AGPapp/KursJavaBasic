2aab0fba04984df8242ec004bec627c405906a6e
Andrey <agpopov@appline.ru>
2022-06-23 21:55:54 +0300

    Второе задание

--- src/ZadanieDva.java
+++ src/ZadanieDva.java
@@ -2,8 +2,5 @@ public class ZadanieDva {
     public static void main(String[] args) {
         double x = 8564168.2323544654654;
         byte a = 127;
-        System.out.println(x);
-        System.out.println(a);
-
-    }
+          }
 }
